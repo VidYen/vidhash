9b8c8a6993dcb219e440ce4a634e37df6d313231
Felty <40067341+VidYen@users.noreply.github.com>
2018-12-12 15:16:16 -0500

    helps if i save

--- vidyen-vidhash/includes/shortcodes/vidyen-vidhash-shortcode.php
+++ vidyen-vidhash/includes/shortcodes/vidyen-vidhash-shortcode.php
@@ -37,9 +37,57 @@ function vidyen_vidhash_video_player_func($atts) {
 
     }
 
-    $youtube_iframe = '<iframe width="560" height="315" src="https://www.youtube.com/embed/f8_FsBQUW_k?controls=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>"';
+    $youtube_html_load = "
+      <!-- 1. The <iframe> (and video player) will replace this <div> tag. -->
+      <div id=\"player\"></div>
 
-    return $youtube_iframe;
+      <script>
+        // 2. This code loads the IFrame Player API code asynchronously.
+        var tag = document.createElement('script');
+
+        tag.src = \"https://www.youtube.com/iframe_api\";
+        var firstScriptTag = document.getElementsByTagName('script')[0];
+        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+        // 3. This function creates an <iframe> (and YouTube player)
+        //    after the API code downloads.
+        var player;
+        function onYouTubeIframeAPIReady() {
+          player = new YT.Player('player', {
+            height: '390',
+            width: '640',
+            videoId: 'M7lc1UVf-VE',
+            events: {
+              'onReady': onPlayerReady,
+              'onStateChange': onPlayerStateChange
+            }
+          });
+        }
+
+        // 4. The API will call this function when the video player is ready.
+        function onPlayerReady(event) {
+          event.target.playVideo();
+        }
+
+        // 5. The API calls this function when the player's state changes.
+        //    The function indicates that when playing a video (state=1),
+        //    the player should play for six seconds and then stop.
+        var done = false;
+        function onPlayerStateChange(event) {
+          if (event.data == YT.PlayerState.PLAYING && !done) {
+            setTimeout(stopVideo, 6000);
+            done = true;
+          }
+        }
+        function stopVideo() {
+          player.stopVideo();
+        }
+      </script>
+      ";
+
+    //$youtube_iframe = '<iframe width="560" height="315" src="https://www.youtube.com/embed/f8_FsBQUW_k?controls=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>"';
+
+    return $youtube_html_load;
 
     //NOTE: Where we are going we don't need $wpdb
     $graphic_choice = $atts['graphic'];
